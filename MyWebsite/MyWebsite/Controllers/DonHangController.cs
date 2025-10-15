c32d9d8d047f3774a182bee959bcd9e65d7c22ee
tung757 <tung24062004@gmail.com>
2025-10-15 20:53:59 +0700

    Thulaibackend

---
+++ MyWebsite/MyWebsite/Controllers/DonHangController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebsite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DonHangController : ControllerBase
+    {
+    }
+}
